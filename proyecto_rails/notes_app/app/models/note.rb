c2de0162172beec9435aefb516c80ad752d3a838
Francisco Di Cicco <francisco.dicicco@gmail.com>
2021-01-24 12:18:00 -0300

    agregue limite para contenido de notas y cambie estilos

--- proyecto_rails/notes_app/app/models/note.rb
+++ proyecto_rails/notes_app/app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ApplicationRecord
   belongs_to :book, inverse_of: :notes
 
   validates :title, presence: true, uniqueness: true, length: { maximum: 255 }, uniqueness: {scope: :book_id}
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 255 }
 
   def to_s
     title
