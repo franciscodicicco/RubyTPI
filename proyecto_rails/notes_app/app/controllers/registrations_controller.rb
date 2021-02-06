efb2626b756b47f284c8bf86cd9a165f0cf5f967
Francisco Di Cicco <francisco.dicicco@gmail.com>
2021-02-06 17:11:19 -0300

    fix en registrations por problema con sign up

--- proyecto_rails/notes_app/app/controllers/registrations_controller.rb
+++ proyecto_rails/notes_app/app/controllers/registrations_controller.rb
@@ -1,6 +1,8 @@
 class RegistrationsController < Devise::RegistrationsController
-    def create
-      super
-      current_user.books.create(user_id: current_user.id, name:"Global Book")
+  def create
+    super
+    if current_user
+      (current_user.books.create(user_id: current_user.id, name:"Global Book"))
     end
+  end
 end
